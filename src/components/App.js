59baea0012d39cbfafd6090119d5ff5943ef0eb4
labradorescence <orangenewyorkcreativeagency@gmail.com>
2022-09-08 15:41:07 -0400

    no post

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
+
 function App() {
+
+  const [search, setSearch] = useState("")
+
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={setSearch}/>
+      <ListingsContainer search={search}/>
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,37 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect }  from "react";
+import ListingCard from "./ListingCard";
+const listingUrl = "http://localhost:6001/listings"
 
-function ListingsContainer() {
+function ListingsContainer({search}) {
+
+  const [listings, setListings] = useState([])
+
+  useEffect(()=>{
+    fetch(listingUrl)
+      .then(resp=> resp.json())
+      .then(data => setListings(data))
+  }, [])
+
+  function onDelete(id){
+    let listingsAfterDelete = listings.filter((listing) => listing.id !== id)
+    setListings(listingsAfterDelete)
+  }
+
+  const eachListing = listings
+    .filter((listing) => listing.description.toLowerCase().includes(search.toLowerCase()))
+    .sort((a, b) => {
+        return a.location.localeCompare(b.location)
+    })
+    .map((listing) => {
+    return (
+      <ListingCard listing = {listing} listingUrl={listingUrl} onDelete={onDelete}/>
+    )
+  })
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {eachListing}
       </ul>
     </main>
   );
